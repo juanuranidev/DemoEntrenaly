2711edf40ce70ad8a1808f97807e42b6cf4a7520
juanuranidev <juanuranidev@gmail.com>
2023-04-08 23:32:37 -0300

    Added initial values to movements

--- util/initialValues.js
+++ util/initialValues.js
@@ -852,3 +852,51 @@ export const trainerInitialValue = {
     },
   ],
 };
+
+export const initialWalletMovements = [
+  {
+    type: 1,
+    amount: 2500,
+    reason: "Pago de plan Diciembre",
+    clientRelated: true,
+    client: "Agustin",
+    observation: "",
+    date: "20/01/2023",
+  },
+  {
+    type: 1,
+    amount: 3000,
+    reason: "Entrenamiento personalizado",
+    clientRelated: true,
+    client: "Carlos",
+    observation: "Se realizó en la casa del cliente",
+    date: "20/01/2023",
+  },
+  {
+    type: 0,
+    amount: 500,
+    reason: "Gasto de transporte",
+    clientRelated: true,
+    client: "Esteban",
+    observation: "Gasto de taxi hasta la casa del cliente",
+    date: "20/01/2023",
+  },
+  {
+    type: 1,
+    amount: 2500,
+    reason: "Pago de plan Diciembre",
+    clientRelated: true,
+    client: "Juan",
+    observation: "",
+    date: "20/01/2023",
+  },
+  {
+    type: 1,
+    amount: 2500,
+    reason: "Pago de plan Enero",
+    clientRelated: true,
+    client: "Nico",
+    observation: "",
+    date: "20/01/2023",
+  },
+];
