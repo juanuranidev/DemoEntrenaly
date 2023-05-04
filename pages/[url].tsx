dcf6f9f40eee93777bca707552d12f0bcc6b3b5a
juanuranidev <juanuranidev@gmail.com>
2023-05-03 23:58:03 -0300

    Working on Url view

--- pages/[url].tsx
+++ pages/[url].tsx
@@ -12,7 +12,7 @@ import {
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import AliceCarousel from "react-alice-carousel";
-import trainerHeader from "assets/images/trainerHeader.png";
+// import trainerHeader from "assets/images/trainerHeader.png";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 export default function Index({ trainer }: any) {
@@ -25,12 +25,34 @@ export default function Index({ trainer }: any) {
     }
   }, []);
 
-  const convertToEmbedUrl = (url: string) => url.replace("watch?v=", "embed/");
-
   return (
     <Container maxW="full" minH="100vh" bg="background.primary" p="0">
+      <Flex justifyContent="center" alignItems="center">
+        <VStack position="absolute">
+          <Heading
+            color="#ffffff"
+            fontSize={{ base: "5xl", md: "6xl", lg: "7xl", xl: "8xl" }}
+          >
+            {trainer?.name} {trainer?.lastName}
+          </Heading>
+          <Text
+            color="#ffffff"
+            fontWeight="600"
+            fontSize={{ base: "md", md: "lg", lg: "xl", xl: "2xl" }}
+          >
+            {trainer?.headline}
+          </Text>
+        </VStack>
+        {/* <Image
+          w="100%"
+          src={trainerHeader.src}
+          h={{ base: "30vh", sm: "40vh", md: "50vh", lg: "80vh", xl: "100vh" }}
+        /> */}
+      </Flex>
       <Flex w="100%" justifyContent="space-around" alignItems="center" py="20">
         <Container
+          // p="5"
+          // mb="10"
           borderRadius="md"
           maxW="container.xl"
           display="flex"
@@ -51,76 +73,43 @@ export default function Index({ trainer }: any) {
             />
           </Flex>
           <VStack w={{ base: "100%", md: "50%" }} spacing="5">
-            <Heading>SOBRE MI</Heading>
-            <Divider />
-            <Text textAlign={{ base: "center", md: "left" }}>
-              {trainer?.description}
-            </Text>
-            <Flex justifyContent="center" w="100%" flexWrap="wrap">
-              {trainer?.instagram ? (
-                <Button mb="5" mx="5" variant="link">
-                  Instagram
-                </Button>
-              ) : null}
-              {trainer?.facebook ? (
-                <Button mb="5" mx="5" variant="link">
-                  Facebook
-                </Button>
-              ) : null}
-              {trainer?.youtube ? (
-                <Button mb="5" mx="5" variant="link">
-                  YouTube
-                </Button>
-              ) : null}
-              {trainer?.whatsapp ? (
-                <Button mb="5" mx="5" variant="link">
-                  WhatsApp
-                </Button>
-              ) : null}
+            {/* <Heading>SOBRE MI</Heading>*/}
+            <Flex
+              h="100%"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Text textAlign={{ base: "center", md: "left" }} fontSize="xl">
+                {trainer?.description}
+              </Text>
+              <Divider />
+              <Flex justifyContent="center" w="100%" flexWrap="wrap">
+                {trainer?.instagram ? (
+                  <Button mb="5" mx="5" variant="link">
+                    Instagram
+                  </Button>
+                ) : null}
+                {trainer?.facebook ? (
+                  <Button mb="5" mx="5" variant="link">
+                    Facebook
+                  </Button>
+                ) : null}
+                {trainer?.youtube ? (
+                  <Button mb="5" mx="5" variant="link">
+                    YouTube
+                  </Button>
+                ) : null}
+                {trainer?.whatsapp ? (
+                  <Button mb="5" mx="5" variant="link">
+                    WhatsApp
+                  </Button>
+                ) : null}
+              </Flex>
             </Flex>
           </VStack>
         </Container>
       </Flex>
-      {trainer?.videos?.length ? (
-        <VStack py="20" w="100%" justifyContent="center">
-          <Container
-            borderRadius="md"
-            maxW="container.xl"
-            justifyContent="center"
-          >
-            <Heading pb="10" textAlign="center">
-              VIDEOS
-            </Heading>
-            <Flex
-              w="100%"
-              flexDirection={{ base: "column", md: "row" }}
-              flexWrap="wrap"
-              justifyContent="center"
-            >
-              {trainer.videos.map((video: any, index: number) => {
-                return (
-                  <Box
-                    display="flex"
-                    key={index}
-                    width={{ base: "100%", md: "30%" }}
-                    height={{ base: "64", md: "52" }}
-                    mr="5"
-                    mb="5"
-                    borderRadius="lg"
-                  >
-                    <iframe
-                      style={{ width: "100%", height: "100%" }}
-                      title="Youtube player"
-                      sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
-                      src={`${convertToEmbedUrl(video.link)}?autoplay=0`}
-                    ></iframe>
-                  </Box>
-                );
-              })}
-            </Flex>
-          </Container>
-        </VStack>
-      ) : null}
       {trainer?.testimonials?.length ? (
         <Flex w="100%" py="20" justifyContent="center" bg="brand.500">
           <Container
@@ -131,9 +120,6 @@ export default function Index({ trainer }: any) {
             maxW="container.xl"
           >
             <VStack w="100%">
-              <Heading pb={{ base: "10", md: "20" }} color="#ffffff">
-                TESTIMONIOS
-              </Heading>
               <AliceCarousel
                 mouseTracking
                 disableDotsControls
@@ -164,7 +150,47 @@ export default function Index({ trainer }: any) {
           </Container>
         </Flex>
       ) : null}
-      <Flex w="100%" mb="10" py="20" justifyContent="center">
+      {trainer?.videos?.length ? (
+        <VStack py="20" w="100%" justifyContent="center">
+          <Container
+            borderRadius="md"
+            maxW="container.xl"
+            justifyContent="center"
+          >
+            {/* <Heading pb="10" textAlign="center">
+              VIDEOS
+            </Heading> */}
+            <Flex
+              w="100%"
+              flexDirection={{ base: "column", md: "row" }}
+              flexWrap="wrap"
+              justifyContent="center"
+            >
+              {trainer.videos.map((video: any, index: number) => {
+                return (
+                  <Box
+                    display="flex"
+                    key={index}
+                    width={{ base: "100%", md: "40%" }}
+                    height={{ base: "64", md: "64" }}
+                    mr="5"
+                    mb="5"
+                    borderRadius="lg"
+                  >
+                    {/* <ReactPlayer
+                      url={video.link}
+                      width="100%"
+                      height="100%"
+                      controls={true}
+                    /> */}
+                  </Box>
+                );
+              })}
+            </Flex>
+          </Container>
+        </VStack>
+      ) : null}
+      {/* <Flex w="100%" mb="10" py="20" justifyContent="center">
         <Container
           borderRadius="md"
           maxW="container.xl"
@@ -185,7 +211,7 @@ export default function Index({ trainer }: any) {
             Iniciar Sesión
           </Button>
         </Container>
-      </Flex>
+      </Flex> */}
     </Container>
   );
 }
